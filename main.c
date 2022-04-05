b354e126f5c981bb3d68b3213a33509d006602cf
Antón L <59964501+anton2403@users.noreply.github.com>
2022-04-05 14:03:02 +0200

    Añadiendo el new (v1, sin probar porque hace falta el stats)

--- main.c
+++ main.c
@@ -11,15 +11,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include "types.h"
+#include "bid_stack.h"
+#include "product_list.h"
 
 #define MAX_BUFFER 255
+//Cabeceras
+void new (char *param1, char *param2, char *param3, char *param4, tItemL *L);
+
+void new (char *param1, char *param2, char *param3, char *param4, tItemL *L) { //Cabecera de la función. Recibe el identificador del nuevo producto (param1), el vendedor del producto (param2), la categoría (param3) y el precio (param4)
+    tItemL newItem;
+
+    if (findItem(param1,L)!=LNULL) { //Si existe un producto en la lista con ese identificador (param1), no podemos insertar otro
+        printf("+ Error: New not possible\n"); //Imprimimos un error para indicar que no se completo la operacion
+    } else {
+        newItem.bidCounter = 0;
+        strcpy(newItem.seller, param2); //Copiamos el valor de param2 en el campo "seller" del struct tItemL d
+        strcpy(newItem.productId, param1); //Copiamos el valor de param1 en el campo "productId" del struct tItemL d
+        if (strcmp(param3, "book")==0){ //el campo "productCategory" pasa a ser book. Si no, pasa a ser "painting" que es el otro tipo posible
+            newItem.productCategory = book;
+            printf("* New: product %s seller %s category book price %s\n", param1, param2, param4);
+        } else {
+            newItem.productCategory = painting; //Si param3 es de tipo book,
+            printf("* New: product %s seller %s category painting price %s\n", param1, param2, param4);
+        }
+        newItem.productPrice = atof(param4); //Pasamos el param4 (char) a float y lo cambiamos en el tItemL d, en el campo del precio
+
+        insertItem (newItem, L);//Insertamos el tItemL d con los valores correspondientes en la lista
+    }
+}
 
 
-
-void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4) {
+void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tItemL *L) {
 
     switch (command) {
         case 'N':
+            new ( *param1,  *param2,  *param3,  *param4,  L);
             break;
         case 'S':
             break;
@@ -38,7 +64,7 @@ void processCommand(char *commandNumber, char command, char *param1, char *param
     }
 }
 
-void readTasks(char *filename) {
+void readTasks(char *filename, lista01) {
     FILE *f = NULL;
     char *commandNumber, *command, *param1, *param2, *param3, *param4;
     const char delimiters[] = " \n\r";
@@ -56,7 +82,7 @@ void readTasks(char *filename) {
             param3 = strtok(NULL, delimiters);
             param4 = strtok(NULL, delimiters);
 
-            processCommand(commandNumber, command[0], param1, param2, param3, param4);
+            processCommand(commandNumber, command[0], param1, param2, param3, param4, lista01);
         }
 
         fclose(f);
@@ -69,6 +95,9 @@ void readTasks(char *filename) {
 
 int main(int nargs, char **args) {
 
+    tList lista01; //Creamos la variable lista01 de tipo tList
+    createEmptyList(&lista01); //Creamos e inicializamos una lista vacia y la almacenamos en la variable lista01
+
     char *file_name = "new.txt";
 
     if (nargs > 1) {
@@ -79,7 +108,7 @@ int main(int nargs, char **args) {
         #endif
     }
 
-    readTasks(file_name);
+    readTasks(file_name, &lista01);
 
     return 0;
 }
